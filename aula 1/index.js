f0db35af79d534b62c85b2c183cfc9cceeec613f
Leonardo Andrighetto <leoandrighetto02@gmail.com>
2025-08-13 22:09:13 -0300

    Add files via upload

---
+++ aula 1/index.js
@@ -0,0 +1,9 @@
+//VARIAVEIS E CONSTANTE
+let variavel = "VARIAVEL";
+const variavel_2 = "CONSTANTE";
+
+console.log(variavel+"\n"+variavel_2);
+console.log("/n")
+
+//npm init - inicia o gerenciamento de pacotes
+
