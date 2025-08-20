4c4bad14b3469918fc2cf2acb36e71afd4a12449
Leonardo Andrighetto <leoandrighetto02@gmail.com>
2025-08-20 20:43:47 -0300

    Create index.js

---
+++ aula_2/index.js
@@ -0,0 +1,39 @@
+    //chamadas assincronas
+    
+    //https://www.npmjs.com/package/readline-sync
+    
+    //escopo estático
+
+
+
+
+//    ENTRADA DE USUÁRIO     ENTRADA DE USUÁRIO     ENTRADA DE USUÁRIO     ENTRADA DE USUÁRIO 
+
+
+
+    // instalação de biblioteca "npm install readline-sync" para obter entrada do usuário de forma síncrona através do termina
+
+    // digitar no terminal: npm install readline-sync
+
+    // carregar import no código: import leitor from 'readline-sync'
+                                  // leitor, é opcional, pode ser qualquer nome
+
+
+
+
+
+
+
+
+
+        //!!!!!ENVIAR APENAS O ARQUIVO index.js e package.json NO DIA DA PROVA!!!!!!!!!
+
+        //!!!!!compacta-los.compacta-los.compacta-los.compacta-los.compacta-los.compacta-los.compacta-los.
+
+        //para instalar dependencias de um arquivo cujos módulos não sejam instalados, 
+
+                  //digitar no terminal: npm install
+
+                  //digitar no código:  import leitor from 'readline-sync';
+
+
