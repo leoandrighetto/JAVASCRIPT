7421ee39fd8e2feb0a32a7564f54cbd8c47a9432
Leonardo Andrighetto <leoandrighetto02@gmail.com>
2025-08-20 21:52:15 -0300

    Update index.js

--- aula 1/index.js
+++ aula 1/index.js
@@ -1,13 +1,17 @@
-//VARIAVEIS E CONSTANTE
-let variavel = "VARIAVEL";
-const variavel_2 = "CONSTANTE";
+// 1 - criar pasta no vs code.
 
-console.log(variavel+"\n"+variavel_2);
-console.log("/n")
+// 2 - transformar pasta em pacote.
 
-// node --version
-//npm init - inicia o gerenciamento de pacotes
-// node index.js
-//cls
+// 3 - linha de type deve ser alterada para "module" ou ADICIONÁ-LA (abaixo de AUTHOR) "type":"module",
 
+// 4- criar arquivo index.js
 
+// 5- npm init - inicia o gerenciamento de pacotes
+
+    // node --version - chega versão do node 
+    // npm --version - checa versão do npm
+
+// no terminal, node index.js para teste (console.log('teste')) - CEMPRE SALVAR O ARQUIVO.
+  
+    // node --watch index.js      - para vigiar o andamento do código.
+    // para sair da area  clica no terminal - ctrl + c
