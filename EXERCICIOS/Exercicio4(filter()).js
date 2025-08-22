9787fc26d8916c0ccb1d5f17cac02872de10575a
Leonardo Andrighetto <leoandrighetto02@gmail.com>
2025-08-22 14:41:45 -0300

    Update Exercicio4(filter()).js

--- EXERCICIOS/Exercicio4(filter()).js
+++ EXERCICIOS/Exercicio4(filter()).js
@@ -1,5 +1,5 @@
-let array = [{'ativo': true}, {'ativo': false}];
+let array = [{ativo: true}, {ativo: false}];
 
-let array_novo = array.filter((dicionario) => dicionario['ativo'] === true);
+let array_novo = array.filter((dicionario) => dicionario["ativo"] === true);
 
 console.log(array_novo);
