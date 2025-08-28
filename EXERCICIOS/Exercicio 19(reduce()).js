b8486ccf139de06fb2e13d865b04f2f147be4407
Leonardo Andrighetto <leoandrighetto02@gmail.com>
2025-08-28 20:56:44 -0300

    Delete EXERCICIOS/Exercicio 19(reduce()).js

--- EXERCICIOS/Exercicio 19(reduce()).js
+++
@@ -1,9 +0,0 @@
-// reduce: Calcule o produto de todos os números de um array.
-// Exemplo: [2, 3, 4] → 24
-
-let array = [2, 3, 4];
-
-let novo_array = array.reduce((acumulador, valor) => acumulador * valor, 1
-);
-
-console.log(novo_array);
