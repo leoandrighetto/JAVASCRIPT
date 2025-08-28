f82e7e698a4f3be82d9aee11f29a4fb498a589da
Leonardo Andrighetto <leoandrighetto02@gmail.com>
2025-08-28 20:57:19 -0300

    Delete EXERCICIOS/Exercicio 10(map()).js

--- EXERCICIOS/Exercicio 10(map()).js
+++
@@ -1,7 +0,0 @@
-//map: Crie um array com a propriedade preço de um array de objetos de produtos.
-
-let array = [{nome: "apple", preco: 2}, {nome: "banana", preco: 3}];
-
-let nova_array = array.map((elemento) => elemento["preco"])
-
-console.log(nova_array)
