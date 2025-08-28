9c98b8863549d4bf6f095c2f758e0e42d53984d6
Leonardo Andrighetto <leoandrighetto02@gmail.com>
2025-08-28 20:57:35 -0300

    Delete EXERCICIOS/Exercicio 11(map()).js

--- EXERCICIOS/Exercicio 11(map()).js
+++
@@ -1,8 +0,0 @@
-// map: Dobre os números pares e triplique os números ímpares de um array.
-// Exemplo: [1, 2, 3] → [3, 4, 9]
-
-let array = [1,2,3,4,5,6];
-
-let nova_array = array.map((i) => (i % 2 === 0 ? i * 2 : i * 3));
-
-console.log(nova_array);
