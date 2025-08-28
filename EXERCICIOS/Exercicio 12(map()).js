fe7d6710b974ebd2d374449f2c2e03edcf45bd6a
Leonardo Andrighetto <leoandrighetto02@gmail.com>
2025-08-28 20:57:51 -0300

    Delete EXERCICIOS/Exercicio 12(map()).js

--- EXERCICIOS/Exercicio 12(map()).js
+++
@@ -1,8 +0,0 @@
-// map: Adicione o prefixo "Item: " a cada string de um array.
-// Exemplo: ["maçã", "banana"] → ["Item: maçã", "Item: banana"]
-
-let array = ["maçã", "banana"];
-
-let nova_array = array.map((item) => "Item: "+ item )
-
-console.log(nova_array);
