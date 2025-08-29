ccf43225ab7662e05992b8e409d33f8ba1abdf8e
Leonardo Andrighetto <leoandrighetto02@gmail.com>
2025-08-28 21:01:26 -0300

    Delete EXERCICIOS/Exercicio9(MAP()).js

--- EXERCICIOS/Exercicio9(MAP()).js
+++
@@ -1,8 +0,0 @@
-// map: Converta cada número de um array para uma string com o sufixo "kg".
-// Exemplo: [2, 5] → ["2kg", "5kg"]
-
-let array = [11,22];
-
-let nova_array = array.map((numero) => (numero + "kg"))
-
-console.log(nova_array)
