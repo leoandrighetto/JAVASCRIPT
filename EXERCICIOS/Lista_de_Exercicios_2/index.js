c9c6f857e12080e7edb2ff2cde779ad3a6794405
Leonardo Andrighetto <leoandrighetto02@gmail.com>
2025-09-05 21:01:48 -0300

    Add files via upload

---
+++ EXERCICIOS/Lista_de_Exercicios_2/index.js
@@ -0,0 +1,72 @@
+// Somar Todos os Elementos de um Array
+
+// let array = [1,2,3,4,5];
+
+// let soma = 0
+// array.forEach((numero) => soma+=numero
+// );
+
+// console.log(soma);
+
+// //Multiplicar Cada Elemento por um Valor Específico
+ 
+// let array = [1,2,3,4];
+// let novo_array = array.map((numero) => numero*2);
+// console.log(novo_array.toString())
+
+// Converter um Array de Celsius para Fahrenheit
+
+// let array = [0,20,30];
+
+// let novo_array = array.map((numero) => (numero*1.8)+32);
+// console.log(novo_array);
+
+//Criar um array de strings de saudação
+
+// let array = ['Leo','Lena','Luc'];
+
+// let saudacoes = array.map((nome) => "Olá, " + nome + "!");
+
+// console.log(saudacoes);
+
+//filtrar números pares 
+
+// let array = [1,2,3,4,5,6];
+
+// let novo_array = array.filter((numero) => numero %2 ===0);
+
+// console.log(novo_array);
+
+//filtrar palavras longas
+
+// let array = ["maçã", "banana", "abacaxi", "uva"];
+
+// let palavraslongas = array.filter((palavras) => palavras.length >= 5);
+
+// console.log(palavraslongas);
+
+
+
+//multiplicar todos os elementos de uma array
+
+// let array = [1,2,3,4];
+
+//  let novo_array = array.reduce((ant,pro) => ant*pro);
+
+//  console.log(novo_array);
+
+
+
+// contar ocorrencias de um valor
+
+// let array = [1,2,2,3,2];
+
+// let contador = 0;
+
+// array.forEach((numero) => numero===2?contador+=1:undefined);
+
+// console.log(contador);
+
+
+
+// MAP E FILTER MAP E FILTER MAP E FILTER MAP E FILTER MAP E FILTER
